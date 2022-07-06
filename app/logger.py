94ff86090473f15799da243fc6eb9563e4c82bdf
eltbus <naselliug@gmail.com>
2022-07-06 18:05:21 +0200

    feat[logging]: logs folder is created automatically

--- app/logger.py
+++ app/logger.py
@@ -1,6 +1,7 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import sys
+import os
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -13,11 +14,15 @@ stderr_handler.setFormatter(formatter)
 stderr_handler.setLevel(logging.WARN)
 logger.addHandler(stderr_handler)
 
+class MakeRotatingFileHandler(RotatingFileHandler):
+    def __init__(self, filename, **kwargs):
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        super().__init__(filename, **kwargs)
+
 try:
-    stats_handler = RotatingFileHandler('logs/stats.log', backupCount=2, maxBytes=1000)
+    stats_handler = MakeRotatingFileHandler('logs/stats.log', backupCount=2, maxBytes=1000)
     stats_handler.setFormatter(formatter)
-    stats_handler.setLevel(logging.INFO)  # type:ignore
-    # stats_handler.addFilter(lambda record: record.levelno == logging.INFO)  # type:ignore
+    stats_handler.setLevel(logging.INFO)
     logger.addHandler(stats_handler)
 except PermissionError:
     logger.warn("Could not set the stats_FileHandler due to a permission error. Don't use both local and docker")
